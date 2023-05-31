62266191868cf54c135a18c773e6570c2d62e345
Siphesihle Mhlongo <127754537+siphesihlemhlongo@users.noreply.github.com>
2023-05-31 18:39:57 +0200

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -1,1 +1,17 @@
 #include 'main.h'
+/*************************PRINT POINTER************************/
+  /**
+   *print_binary - prints the unsigned number
+   *@types: list of the arguments
+   *@flags: calculates the active flags
+   *@buffer: buffer array to handle the print
+   *@width: get the width
+   *@precision:specification of the precision
+   *@size: specify the size
+   *Return: the number of the chars printed
+   */
+int print_pointer(va_list types, char buffer[],
+                  int flags, int width, int precision, int size)
+{
+  char extra_c = 0, padd = ' ';
+  int ind =
