c78b6f2e1132158da2a704423a62738f88b55478
Siphesihle Mhlongo <127754537+siphesihlemhlongo@users.noreply.github.com>
2023-06-01 03:25:10 +0200

    Update size.c

--- size.c
+++ size.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
+ * get_size - Calculates the size that cast the argument
+ * @format: Formatted the string in which to print the arguments
+ * @i: the List of arguments to be printed.
  * Return: Precision.
  */
 int get_size(const char *format, int *i)
