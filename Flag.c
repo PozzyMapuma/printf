3e1edc5ce3ffa9701c28e39fceb74d9feb4053ff
Siphesihle Mhlongo <127754537+siphesihlemhlongo@users.noreply.github.com>
2023-06-01 03:28:54 +0200

    Update Flag.c

--- Flag.c
+++ Flag.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * get_flags - Calculates the active flags
+ * @format: Formatted the string in which to print the arguments
+ * @i: take an parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
